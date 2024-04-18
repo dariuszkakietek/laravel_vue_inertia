db55c06eeb3a4298a5531a4c7f4573938e3a5083
Dariusz <dariuszkakietek@gmail.com>
2024-04-18 10:53:43 +0200

    monthly payment calculator

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -13,8 +13,55 @@
                 <ListingAddress :listing="listing" class="text-gray-500"/>
             </Box>
             <Box>
-                <template #header>Offer</template>
-                Make an offer
+                <template #header>Monthly Payment</template>
+                <div>
+                    <label for="">Interest rate ({{ interestRate }} %)</label>
+                    <input
+                        type="range"
+                        min="0.1"
+                        max="30"
+                        step="0.1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                        v-model.number="interestRate"
+                    />
+
+                    <label for="">Duration ({{ duration }} years)</label>
+                    <input
+                        type="range"
+                        min="3"
+                        max="35"
+                        step="1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                        v-model.number="duration"
+                    />
+
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">You monthly payment</div>
+                        <Price :price=monthlyPayment class="text-3xl" />
+                    </div>
+
+                    <div class="text-gray-500 mt-2">
+                        <div class="flex justify-between">
+                            <div class="">Total paid</div>
+                            <Price :price="totalPaid" />
+                        </div>
+                    </div>
+
+                    <div class="text-gray-500 mt-2">
+                        <div class="flex justify-between">
+                            <div class="">Principal paid</div>
+                            <Price :price="listing.price" />
+                        </div>
+                    </div>
+
+                    <div class="text-gray-500 mt-2">
+                        <div class="flex justify-between">
+                            <div class="">Interest paid</div>
+                            <Price :price="totalInterest" />
+                        </div>
+                    </div>
+
+                </div>
             </Box>
         </div>
     </div>
@@ -28,9 +75,18 @@ import Price from '@/Components/Price.vue';
 import ListingSpace from '@/Components/ListingSpace.vue';
 import Box from '@/Components/UI/Box.vue';
 
-defineProps({
+import { ref } from 'vue';
+
+import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
+
+const interestRate = ref(2.5);
+const duration = ref(25);
+
+const props = defineProps({
     listing: Object,
 })
+
+const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(props.listing.price, interestRate, duration);
 
 </script>
 
